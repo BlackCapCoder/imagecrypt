0f4d9b4af7ec3510d2dc084599fed916a2f21eab
BlackCap <mailhph@gmail.com>
2018-02-06 23:05:25 +0100

    golfed down js some more

--- js/dec.js
+++ js/dec.js
@@ -1,4 +1,3 @@
 for(l of require('fs').readFileSync(0).toString().split`
 `){c=l.split` `[3]
-if(c>254)break
-process.stdout.write(String.fromCharCode(255-c+96))}
+if(c<255)process.stdout.write(String.fromCharCode(255-c+96))}
