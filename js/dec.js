208923770cc58959f39d59b27e18f76844fcefe0
BlackCap <mailhph@gmail.com>
2018-02-06 23:21:13 +0100

    golfed down js

--- js/dec.js
+++ js/dec.js
@@ -1,3 +1,2 @@
-for(l of require('fs').readFileSync(0).toString().split`
-`){c=l.split` `[3]
-if(c<255)process.stdout.write(String.fromCharCode(255-c+96))}
+require('fs').readFileSync(0).toString().split`
+`.map(x=>(c=x.split` `[3])<255?process.stdout.write(String.fromCharCode(255-c+96)):0)
--- js/enc.js
+++ js/enc.js
@@ -1,3 +1,2 @@
-m='tematisrolodmuspimerol'.split``.map(x=>255-x.charCodeAt(0)+96)
-for(l of require('fs').readFileSync(0).toString().split`
-`)console.log(l+' '+(m.pop()||255))
+require('fs').readFileSync(0).toString().split`
+`.map((l,i)=>console.log(l+' '+(i>21?255:255+96-'loremipsumdolorsitamet'.split``[i].charCodeAt(0))))
